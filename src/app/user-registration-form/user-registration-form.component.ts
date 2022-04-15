5b9a7f9dca7988fd66f7205ed05ad1d8e68019a7
LukeHTHompson <lht14dv@icloud.com>
2022-04-15 17:55:01 -0400

    Registration Confirm Message

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -38,6 +38,7 @@ export class UserRegistrationFormComponent implements OnInit {
       // Logic for a successful user registration goes here! (To be implemented)
       this.dialogRef.close(); // This will close the modal on success!
       console.log(result);
+      let readableResult = result.Username + ' has been successfully created!'
       this.snackBar.open(result, 'OK', {
         duration: 2000
       });
